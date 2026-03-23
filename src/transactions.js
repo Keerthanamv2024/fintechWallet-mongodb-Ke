b5b9de423d3fe3fad06a4cf35e9e9801d2fe7e35
Keerthanamv2024 <keerthana.mv.2024.it@rajalakshmi.edu.in>
2026-03-23 23:35:13 +0530

    Add files via upload

---
+++ src/transactions.js
@@ -0,0 +1,6 @@
+db.transactions.insertOne({
+  fromUser: ObjectId('69c16c06c00b3b8189267419'), // Ezhil
+  toUser: ObjectId('69c16c06c00b3b8189267418'),   // Keerthana
+  amount: 100,
+  date: new Date()
+})
